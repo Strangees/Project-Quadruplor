2be315a1eb9ad762eb1a494cfe7b94f665af2820
stian.strange <stianstrange@gmail.com>
2018-01-26 15:05:55 +0100

    1st commit

--- app/src/main/java/com/example/stianstrange/project_quadruplor/SignUp.java
+++ app/src/main/java/com/example/stianstrange/project_quadruplor/SignUp.java
@@ -22,8 +22,6 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -50,34 +48,6 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
         findViewById(R.id.buttonSignUp).setOnClickListener(this);
         findViewById(R.id.textViewLogin).setOnClickListener(this);
-    }
-
-    public void AddFirestoreUser() {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        // Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("Email",editTextEmail);
-        user.put("Template","Null");
-        final String TAG = "Firestore";
-
-
-        // Add a new document with a generated ID
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG,"DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
-
-
     }
 
 
