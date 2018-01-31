50f4eb47ebe28edd7c2c64fad12996752f669681
stian.strange <stianstrange@gmail.com>
2018-01-31 14:10:45 +0100

    2nd commit

---
+++ app/src/main/java/com/example/stianstrange/project_quadruplor/Content.java
@@ -0,0 +1,190 @@
+package com.example.stianstrange.project_quadruplor;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.PopupWindow;
+import android.app.AlertDialog;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryListenOptions;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Calendar;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+
+import static com.gjiazhe.layoutswitch.ItemAdapter.SPAN_COUNT_ONE;
+
+/**
+ * Created by stian.strange on 27.01.2018.
+ */
+
+public class Content extends AppCompatActivity implements View.OnClickListener{
+    //Declarations
+    FirebaseAuth mAuth;
+    TextView ChatView;
+    EditText ChatSend;
+    ProgressBar progressBar;
+    RecyclerView recyclerView;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content);
+        progressBar = findViewById(R.id.progressbar);
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        ChatSend = findViewById(R.id.ChatSend);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv);
+
+        //On click listener
+        findViewById(R.id.buttonSend).setOnClickListener(this);
+
+
+        progressBar.setVisibility(View.GONE);
+        recievetextlive();
+    }
+
+
+    private void sendtext(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String, Object> message = new HashMap<>();
+
+        //strings
+        String textsend = ChatSend.getText().toString().trim();
+        String Uid = mAuth.getCurrentUser().getUid().toString();
+        Date currentTime = Calendar.getInstance().getTime(); //gets current time
+        String TS = currentTime.toString().trim();
+
+        //puts
+        message.put("UserID",Uid);
+        message.put("Message",textsend);
+        message.put("Timestamp",TS); //make this firestore server timestamp
+        message.put("Group","support");
+
+
+
+// Add a new document with a generated ID
+        db.collection("message")
+                .add(message)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        String FS = "Firestoredata";
+                        Log.d(FS, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        String FS = "Firestoredata";
+                        Log.w(FS, "Error adding document", e);
+                    }
+                });
+
+    }
+
+    private void recievetextlive() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        final String TAG = "FIREBASE RECIEVE";
+        db.collection("message")
+                .whereEqualTo("Group", "support")
+                .orderBy("Timestamp", Query.Direction.DESCENDING)
+                .limit(50)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot info,
+                                        @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.w(TAG, "Listen failed.", e);
+                            return;
+                        }
+
+                        List<String> message = new ArrayList<String>();
+                        for (DocumentSnapshot doc : info) {
+                            if (doc.get("Group") != null) {
+                                message.add(doc.getString("Message"));
+
+                            }
+                        }
+                        Log.d(TAG, "Messages: " + message);
+
+                    }
+                });
+    }
+
+
+
+
+//    private void recievetextsingletime() {
+//       FirebaseFirestore db = FirebaseFirestore.getInstance();
+//        db.collection("message").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>()
+//        {
+//            String TAG = "FIREBASE RECIEVE";
+//            String textrecieve = ChatView.getText().toString().trim();
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (DocumentSnapshot document : task.getResult()) {
+//                                Log.d(TAG, document.getId() + " => " + document.getData().toString());
+//                                //recycler();
+//                            }
+//                        } else {
+//                            Log.d(TAG, "Error getting documents: ", task.getException());
+//                        }
+//                    }
+//                });
+//
+//    }
+
+
+
+    public void onClick(View view) {
+        sendtext();
+
+    }
+}
+
+
